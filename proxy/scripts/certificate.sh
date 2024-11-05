fa99b8395b87da19c1b9ee6dcf144508dd2bcd23
minsang8332 <minsang8332@gmail.com>
2024-11-05 20:14:36 +0900

    feat: 프록시 인증서 발급 및 갱신 크론 설정

--- proxy/scripts/certificate.sh
+++ proxy/scripts/certificate.sh
@@ -1,1 +1,42 @@
-# TODO
+#!/bin/bash
+STAGE_CA="https://acme-staging-v02.api.letsencrypt.org"
+REAL_CA="https://acme-v02.api.letsencrypt.org"
+exists_domain () {
+    ip_list=$(dig +short $1)
+    if [ -z "$ip_list" ]; then
+        exit 1
+    fi
+    for ip in $ip_list; do
+        ping -c 3 -W 3 $IP > /dev/null
+        if [ $? -eq 0 ]; then
+            exit 0
+        fi
+    done
+    exit 2
+}
+make_certfile () {
+    ca="$1"
+    cat <<-EOL > getssl.cfg
+        CA="$ca"
+        ACL=('/var/www/html/.well-known/acme-challenge')
+        USE_SINGLE_ACL="true"
+EOL
+    ./getssl "$ROOT_DOMAIN" -q -U >> getssl.log 2>&1
+}
+# 도메인 존재하는지 확인
+exists_domain "$ROOT_DOMAIN"
+if [ ! $? -eq 0 ]; then
+    exit 255
+fi
+# 스테이지 파일 생성되면 찐으로 발급
+make_certfile "$STAGE_CA"
+if [ -f "./$ROOT_DOMAIN/fullchain.pem" ]; then
+    rm -rf $ROOT_DOMAIN
+    make_certfile "$REAL_CA"
+    tmp="/etc/nginx/_conf.d"
+    if [ -d "$tmp" ]; then
+        mv "$tmp" /etc/nginx/conf.d && nginx -s reload
+    fi
+    exit 0
+fi;
+exit 254
---
+++ proxy/scripts/initialize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+# 템플릿 환경변수 치환
+templates=("/etc/nginx/" "/etc/nginx/conf.d/")
+for template in "${templates[@]}"; do
+    for config in "$template"*.conf; do
+        if [[ -f "$config" ]]; then
+            envsubst '${SSL_CERTIFICATE} ${ROOT_DOMAIN}' < "$config" > "$config.tmp"
+            mv "$config.tmp" "$config"
+        fi
+    done
+done
+# acme-callenge 권한 설정
+mkdir -p /var/www/html/.well-known/acme-challenge
+chmod -R 755 /var/www/html/.well-known
+# 스크립트가 위치한 폴더로 이동하여 실행 권한 부여
+cd /home/user/scripts
+# getssl 다운로드
+wget https://raw.githubusercontent.com/srvrco/getssl/master/getssl
+chmod +x *
+# cron 설정
+mkdir -p /root/.cache/crontab
+echo "0 0 1 * * /home/user/scripts/certificate.sh > /dev/null 2>&1" | crontab -
